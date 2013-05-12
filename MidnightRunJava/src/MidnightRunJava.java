c3327961d4d9adaefbabdc35e08da0d2c3704c40
Fausto Ruvalcaba <se.fausto@gmail.com>
2013-05-12 02:10:21 -0700

    Corrected drawing of rectangle and added Polygon

--- MidnightRunJava/src/MidnightRunJava.java
+++ MidnightRunJava/src/MidnightRunJava.java
@@ -23,51 +23,76 @@ public class MidnightRunJava extends JFrame {
 	public void paint(Graphics g) {
 		super.paint(g);
 
-		HashMap<Object[], java.math.BigDecimal> shapes = GetShapes();
+		HashMap<Object[], String> shapes = GetShapes();
 
-		Iterator<Entry<Object[], BigDecimal>> it = shapes.entrySet().iterator();
+		Iterator<Entry<Object[], String>> it = shapes.entrySet().iterator();
 		while (it.hasNext()) {
-			Map.Entry<Object[], java.math.BigDecimal> pairs = (Map.Entry<Object[], BigDecimal>) it
+			Map.Entry<Object[], String> pairs = (Map.Entry<Object[], String>) it
 					.next();
 
-			if (pairs.getValue().equals(java.math.BigDecimal.valueOf(2)))
+			if (pairs.getValue().equals("Line")) {
 				drawOracleMultiline(g, pairs);
-			else if (pairs.getValue()
-					.equals(java.math.BigDecimal.valueOf(1003))) {
-				List<Integer> xPoints = new ArrayList<Integer>();
-				List<Integer> yPoints = new ArrayList<Integer>();
-
-				for (int i = 0; i < pairs.getKey().length; i++) {
-					xPoints.add(convertOracleXToJavaX((BigDecimal) pairs.getKey()[i]));
-					yPoints.add(convertOracleYToJavaY((BigDecimal) pairs.getKey()[++i]));
-
-				}
-
-				// for a rectangle it goes from starting point to width and
-				// height
-				// so do the math
-				int x = xPoints.get(0);
-				int y = yPoints.get(0);
-				
-				int width = xPoints.get(1) - xPoints.get(0) ;
-				int height = yPoints.get(1) - yPoints.get(0);
-				
-				g.drawRect(x, y, width, height);
-
+			} else if (pairs.getValue().equals("Rectangle")) {
+				drawOracleRectangle(g, pairs);
+			} else if (pairs.getValue().equals("Polygon")) {
+				drawOraclePolygon(g, pairs);
+			} else if (pairs.getValue().equals("Curve")) {
 			}
 		}
 
 	}
 
+	private void drawOraclePolygon(Graphics g, Map.Entry<Object[], String> pairs) {
+		List<Integer> xPoints = new ArrayList<Integer>();
+		List<Integer> yPoints = new ArrayList<Integer>();
+
+		for (int i = 0; i < pairs.getKey().length; i++) {
+			xPoints.add(convertOracleXToJavaX((BigDecimal) pairs.getKey()[i]));
+			yPoints.add(convertOracleYToJavaY((BigDecimal) pairs.getKey()[++i]));
+
+		}
+
+		int[] x = new int[xPoints.size()];
+		int[] y = new int[yPoints.size()];
+
+		for (int i = 0; i < xPoints.size(); i++) {
+			x[i] = xPoints.get(i);
+			y[i] = yPoints.get(i);
+		}
+
+		g.drawPolygon(x, y, x.length);
+	}
+
+	private void drawOracleRectangle(Graphics g,
+			Map.Entry<Object[], String> pairs) {
+		List<BigDecimal> xPoints = new ArrayList<BigDecimal>();
+		List<BigDecimal> yPoints = new ArrayList<BigDecimal>();
+
+		for (int i = 0; i < pairs.getKey().length; i++) {
+			xPoints.add((BigDecimal) pairs.getKey()[i]);
+			yPoints.add((BigDecimal) pairs.getKey()[++i]);
+
+		}
+
+		// for a rectangle it goes from starting point to width and
+		// height so do the math
+		int x = convertOracleXToJavaX(xPoints.get(0));
+		int y = convertOracleYToJavaY(yPoints.get(1));
+
+		int width = convertOracleXToJavaX(xPoints.get(1)) - x;
+		int height = (yPoints.get(1).intValue() - yPoints.get(0).intValue())
+				* multiplier;
+		g.drawRect(x, y, width, height);
+	}
+
 	private void drawOracleMultiline(Graphics g,
-			Map.Entry<Object[], java.math.BigDecimal> pairs) {
+			Map.Entry<Object[], String> pairs) {
 		for (int i = 0; i < pairs.getKey().length; i++) {
 			int x1 = convertOracleXToJavaX((BigDecimal) pairs.getKey()[i]);
 			int y1 = convertOracleYToJavaY((BigDecimal) pairs.getKey()[++i]);
 
 			int x2 = convertOracleXToJavaX((BigDecimal) pairs.getKey()[++i]);
 			int y2 = convertOracleYToJavaY((BigDecimal) pairs.getKey()[++i]);
-
 			g.drawLine(x1, y1, x2, y2);
 		}
 	}
@@ -80,13 +105,13 @@ public class MidnightRunJava extends JFrame {
 
 	}
 
-	private static HashMap<Object[], java.math.BigDecimal> GetShapes() {
-		HashMap<Object[], java.math.BigDecimal> result = new HashMap<Object[], java.math.BigDecimal>();
+	private static HashMap<Object[], String> GetShapes() {
+		HashMap<Object[], String> result = new HashMap<Object[], String>();
 
 		Statement stmt = null;
 		Connection con = null;
 
-		String query = "SELECT m.Shape.sdo_elem_info as info, m.Shape.sdo_ordinates as ordinates from MVDEMO.MidnightRun m where id=10 or id =1 or id =2 ";
+		String query = "SELECT m.Shape.sdo_elem_info as info, m.Shape.sdo_ordinates as ordinates from MVDEMO.MidnightRun m where id=10 or id =1 or id =2 or id=11 or id=12";
 		try {
 
 			DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
@@ -104,8 +129,25 @@ public class MidnightRunJava extends JFrame {
 				Object[] elemInfo = (Object[]) infoArray.getArray();
 
 				Array ordinateArray = rs.getArray("ordinates");
-				result.put((Object[]) ordinateArray.getArray(),
-						(java.math.BigDecimal) elemInfo[1]);
+
+				String type = "";
+				if (((java.math.BigDecimal) elemInfo[1])
+						.equals(java.math.BigDecimal.valueOf(2))) {
+					if (((java.math.BigDecimal) elemInfo[2])
+							.equals(java.math.BigDecimal.valueOf(2))) {
+						type = "Curve";
+					} else {
+						type = "Line";
+					}
+				} else if (((java.math.BigDecimal) elemInfo[1])
+						.equals(java.math.BigDecimal.valueOf(1003))) {
+					type = "Rectangle";
+				} else if (((java.math.BigDecimal) elemInfo[1])
+						.equals(java.math.BigDecimal.valueOf(1005))) {
+					type = "Polygon";
+				}
+
+				result.put((Object[]) ordinateArray.getArray(), type);
 			}
 
 		} catch (SQLException e) {
@@ -118,7 +160,6 @@ public class MidnightRunJava extends JFrame {
 
 	private static int convertOracleYToJavaY(BigDecimal oracleY) {
 		return convertOracleYToJavaY(oracleY.intValue());
-
 	}
 
 	private static int convertOracleYToJavaY(int oracleY) {
